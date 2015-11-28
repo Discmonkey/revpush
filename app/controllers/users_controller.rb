ec3705c9d0fccf27c01168a4a5feb24a20421a1c
Max Grinchenko <max@Maxs-MacBook-Air.local>
2015-11-28 16:57:01 -0500

    all problems solved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #flash upon sucess only display once redirect_to automatically figures out to route to the user url
+      #flash upon success only display once redirect_to automatically figures out to route to the user url
       flash[:success] = "Welcome to the sample app!"
       redirect_to @user
     else
