6268505974aab26e55af9d182bd4440114e2a98d
max grinchenko <frismo98@gmail.com>
2015-12-10 20:54:41 -0500

    changed about page

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -4,6 +4,13 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   include ApplicationHelper
   def setup
     @user = users(:max)
+    @user2 = users(:archer)
   end
+
+  test "users can only start movements from thier own page" do
+
+  end
+
+
 
 end
